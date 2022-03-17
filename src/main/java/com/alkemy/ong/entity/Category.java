79bed6cc32883a9d8b42a7a11cf7acb13ceb472b
Juani Runco <juan.runcocarreno@fce.uncu.edu.ar>
2022-03-17 12:01:15 -0300

    OT169-18 Category entity and repository added

---
+++ src/main/java/com/alkemy/ong/entity/Category.java
@@ -0,0 +1,43 @@
+package com.alkemy.ong.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE categories SET soft_delete = true WHERE id=?")
+@Where(clause = "soft_delete=false")
+public class Category {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+    @NonNull
+    private String name;
+    @Nullable
+    private String description;
+    @Nullable
+    private String image;
+    private Timestamp timestamp = Timestamp.from(Instant.now());
+    @Column(name = "soft_delete")
+    private boolean softDelete = Boolean.FALSE;
+
+    public Category(String name, String description, String image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+}
