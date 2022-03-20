5cdde267fbe70b840e501b77832151cf73259739
Silvia R. Otaka <srotaka@gmail.com>
2022-03-20 18:32:04 -0300

    EmailService created.
    Welcome email template created.
    sendWelcomeMail() method created.
    
    This commit is part of three tickets combined:  OT169-27, OT169-29, OT169-30.

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -1,13 +1,27 @@
 package com.alkemy.ong;
 
+import com.alkemy.ong.service.EmailService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class OngApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(OngApplication.class, args);
+
 	}
 
+	// Esta parte es solo para hacer la prueba del método. Colocar un mail real para comprobar la recepción
+	@Bean
+	CommandLineRunner run(EmailService emailService) {
+		return args -> {
+			emailService.sendWelcomeMail("mail_generico@mail_generico.com");
+		};
+
+	}
 }
