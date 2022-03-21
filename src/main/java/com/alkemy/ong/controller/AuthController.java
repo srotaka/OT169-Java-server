021a736fe7217d349c282784d7211a0ec51bc2a1
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-03-21 11:37:06 -0300

    AuthController modified: User entity added

--- src/main/java/com/alkemy/ong/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/controller/AuthController.java
@@ -1,9 +1,16 @@
 package com.alkemy.ong.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alkemy.ong.entity.User;
+import com.alkemy.ong.repository.UserRepository;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,19 +22,19 @@ public class AuthController {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
-	/*
+	
 	@Autowired 
 	private UserRepository userRepository; //This repository doesn't exists in this commit 
 	
 	@PostMapping("/register")
 	public ResponseEntity<User> register(@RequestBody User user){ //Not functional. User doesn't exists
 		User obj = new User(); 
-		obj.setNombre(user.getNombre());
-		obj.setApellido(user.getApellido());
+		obj.setFirstName(user.getFirstName());
+		obj.setLastName(user.getLastName());
 		obj.setEmail(user.getEmail());
-		obj.setContrasena( passwordEncoder.encode(user.getContrasena()) );
+		obj.setPassword( passwordEncoder.encode(user.getPassword()) );
 		userRepository.save(obj);//This repository doesn't exists in this commit 
 		return new ResponseEntity<User>(obj, HttpStatus.OK);
 	}
-	*/
+	
 }
