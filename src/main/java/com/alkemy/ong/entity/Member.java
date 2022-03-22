89434d663f93436fc7d956d6d05602905c9f7c95
Facundo_98 <facu98ortega@gmail.com>
2022-03-22 13:25:42 -0300

    Change Member id to String type

---
+++ src/main/java/com/alkemy/ong/entity/Member.java
@@ -0,0 +1,51 @@
+package com.alkemy.ong.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "members")
+@SQLDelete(sql = "UPDATE membres SET soft_delete = TRUE WHERE id=?")
+@Where(clause = "soft_delete = false")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+
+	@Column(nullable = false)
+	private String name;
+
+	private String facebookUrl;
+
+	private String instagramUrl;
+
+	private String linkedinUrl;
+
+	@Column(nullable = false)
+	private String image;
+
+	private String description;
+
+	private Timestamp timestamp = Timestamp.from(Instant.now());
+
+	@Column(name = "soft_delete")
+	private boolean softDelete = Boolean.FALSE;
+}
--- src/main/java/com/alkemy/ong/repository/MemberRepository.java
+++ src/main/java/com/alkemy/ong/repository/MemberRepository.java
@@ -1,13 +1,10 @@
 package com.alkemy.ong.repository;
 
-import java.util.UUID;
-
+import com.alkemy.ong.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.alkemy.ong.entity.Activity;
-
 @Repository
-public interface ActivityRepository extends JpaRepository<Activity, UUID>{
+public interface MemberRepository extends JpaRepository<Member, String>{
 	
 }
