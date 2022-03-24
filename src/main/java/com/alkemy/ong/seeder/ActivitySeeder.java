4ab9efb27a12f865bb1be0e1811f9bd02b2d46c6
Hernan <hernan.olmos11@gmail.com>
2022-03-24 10:45:15 -0300

    OT169-52 added new method to create default activities and changed the data to match the real one.

--- src/main/java/com/alkemy/ong/seeder/ActivitySeeder.java
+++ src/main/java/com/alkemy/ong/seeder/ActivitySeeder.java
@@ -19,13 +19,38 @@ public class ActivitySeeder implements CommandLineRunner {
 
     private void seedTheDataBaseWithActivitiesIfItsEmpty() {
         if(repository.count() == 0) {
-            Activity firstActivity = new Activity("First Activity", "The content", "firstActivity.jpg");
-            Activity secondActivity = new Activity("Second Activity", "The content", "secondActivity.jpg");
-            Activity thirdActivity = new Activity("Third Activity", "The content", "thirdActivity.jpg");
-
-            repository.save(firstActivity);
-            repository.save(secondActivity);
-            repository.save(thirdActivity);
+            Activity[] activities = createDefaultActivities();
+            for(Activity a: activities) {
+                repository.save(a);
+            }
         }
+    }
+
+    private Activity[] createDefaultActivities() {
+
+        Activity[] activities = new Activity[3];
+
+        activities[0] = new Activity(
+                "Apoyo Escolar para el nivel Primario",
+                "El espacio de apoyo escolar es el corazón del área educativa. Se realizan los\n" +
+                        "talleres de lunes a jueves de 10 a 12 horas y de 14 a 16 horas en el\n" +
+                        "contraturno.",
+                "shorturl.at/btCH4");
+
+        activities[1] = new Activity(
+                "Apoyo Escolar Nivel Secundario",
+                "Del mismo modo que en primaria, este taller es el corazón del área\n" +
+                        "secundaria. Se realizan talleres de lunes a viernes de 10 a 12 horas y de 16 a\n" +
+                        "18 horas en el contraturno.",
+                "shorturl.at/boGI8");
+
+        activities[2] = new Activity(
+                "Tutorías",
+                "Es un programa destinado a jóvenes a partir del tercer año de secundaria,\n" +
+                        "cuyo objetivo es garantizar su permanencia en la escuela y construir un\n" +
+                        "proyecto de vida que da sentido al colegio.",
+                "shorturl.at/epPQ6");
+
+        return activities;
     }
 }
