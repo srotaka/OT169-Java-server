ce0cdd03f69c4ec24fe868b506259c92845bf5d0
Hernan <hernan.olmos11@gmail.com>
2022-03-25 17:07:35 -0300

    OT169-50 changed Mapper to receive 2 activity arguments, and updated service

--- src/main/java/com/alkemy/ong/utils/Mapper.java
+++ src/main/java/com/alkemy/ong/utils/Mapper.java
@@ -17,8 +17,7 @@ public class Mapper {
 		return dto;
 	}
 
-	public static ActivityDto mapToDto(Activity activity) {
-		ActivityDto dto = new ActivityDto();
+	public static ActivityDto mapToDto(Activity activity, ActivityDto dto) {
 		dto.setId(activity.getId());
 		dto.setName(activity.getName());
 		dto.setContent(activity.getContent());
@@ -26,9 +25,7 @@ public class Mapper {
 		return dto;
 	}
 
-	public static Activity mapFromDto(ActivityDto dto) {
-		Activity activity = new Activity();
-		activity.setId(dto.getId());
+	public static Activity mapFromDto(ActivityDto dto, Activity activity) {
 		activity.setName(dto.getName());
 		activity.setContent(dto.getContent());
 		activity.setImage(dto.getImage());
