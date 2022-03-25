0dff115f3225c58ce32063f7ea6a48a86f71f3bc
Hernan <hernan.olmos11@gmail.com>
2022-03-25 11:33:34 -0300

    OT169-37 created organization/public endpoint to do a POST request and update the organization information

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -2,7 +2,6 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.OrganizationRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,17 +19,13 @@ public class OrganizationController {
 	
 	@Autowired
 	private OrganizationService service;
-
-	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+	
 	@GetMapping("/public") 
 	public OrganizationResponseDto getPublicInfo() {
 		return service.getPublicInfo();
 	}
 
-	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping("/public")
-	public OrganizationResponseDto postPublicInfo(@RequestBody @Valid OrganizationRequestDto organizationRequestDto) {
-		return service.postPublicInfo(organizationRequestDto);
-	}
+	public OrganizationResponseDto postPublicInfo(@RequestBody @Valid OrganizationRequestDto organizationRequestDto) { return service.postPublicInfo(organizationRequestDto); }
 
 }
