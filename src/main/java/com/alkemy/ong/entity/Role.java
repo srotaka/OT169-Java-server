3757d34122a37987841a7aaad202994f7dcbc613
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-03-25 19:29:49 -0300

    Rebasing

--- src/main/java/com/alkemy/ong/entity/Role.java
+++ src/main/java/com/alkemy/ong/entity/Role.java
@@ -4,6 +4,10 @@ import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+<<<<<<< HEAD
+=======
+import javax.persistence.Column;
+>>>>>>> 3f4d31c (Unidirectional User->Role)
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -26,13 +30,20 @@ import lombok.Setter;
 @AllArgsConstructor// Added by Franco Lamberti
 @NoArgsConstructor// Added by Franco Lamberti
 @Table(name="role")
+<<<<<<< HEAD
 @SQLDelete(sql = "UPDATE role SET softDeleted = true WHERE id=?")
 @Where(clause="deleted=false")
+=======
+@SQLDelete(sql = "UPDATE role SET soft_deleted = true WHERE id=?")
+@Where(clause="soft_deleted = false")
+
+>>>>>>> 3f4d31c (Unidirectional User->Role)
 public class Role {
 
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "role_id")
     private String id;
 
     private String name;
@@ -44,6 +55,20 @@ public class Role {
 
     private boolean softDeleted = Boolean.FALSE;
 
+<<<<<<< HEAD
+=======
+   /*
+    @OneToMany(
+			mappedBy = "role",
+			cascade = CascadeType.ALL,
+			orphanRemoval = true
+			)			
+			private List<User> users;
+    */
+    
+    
+    
+>>>>>>> 3f4d31c (Unidirectional User->Role)
     //Added by Franco Lamberti
     public Role(String id) {
     	this.id = id;
--- src/main/java/com/alkemy/ong/entity/User.java
+++ src/main/java/com/alkemy/ong/entity/User.java
@@ -1,12 +1,29 @@
 package com.alkemy.ong.entity;
 
+<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+=======
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+>>>>>>> 3f4d31c (Unidirectional User->Role)
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+<<<<<<< HEAD
 import javax.persistence.Entity;
 import javax.persistence.Column;
 import javax.persistence.Table;
@@ -18,6 +35,14 @@ import javax.persistence.ForeignKey;
 import javax.persistence.CascadeType;
 import java.sql.Timestamp;
 import java.time.Instant;
+=======
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+>>>>>>> 3f4d31c (Unidirectional User->Role)
 
 @Entity
 @Table(name = "users")
@@ -26,11 +51,13 @@ import java.time.Instant;
 @NoArgsConstructor
 @SQLDelete(sql = "UPDATE users SET soft_delete = TRUE WHERE id=?")
 @Where(clause = "soft_delete = false")
+
 public class User {
 	
 	@Id
 	@GeneratedValue(generator = "uuid")
 	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	@Column(name = "user_id")
 	private String id;
 	
     @Column(nullable = false)
@@ -48,11 +75,21 @@ public class User {
     private String photo;
 
     private Timestamp timestamp = Timestamp.from(Instant.now());
+<<<<<<< HEAD
 
     
     @JoinColumn(name = "role_id",nullable = false,foreignKey=@ForeignKey(name = "Fk_role_id"))
     @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REFRESH})
     private Role roleId;
+=======
+/*
+    @ManyToOne(fetch = FetchType.LAZY ,cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REFRESH})
+    @JoinColumn(name = "users",nullable = false)
+    */
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+	@JoinColumn(name = "role_id")
+    private Role role;//changed "_"
+>>>>>>> 3f4d31c (Unidirectional User->Role)
     
 
     @Column(name = "soft_delete")
