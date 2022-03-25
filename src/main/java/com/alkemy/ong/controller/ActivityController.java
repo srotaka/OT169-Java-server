26800cd7f9a38dec4f3c0b5dbd5f7ed714beda65
Hernan <hernan.olmos11@gmail.com>
2022-03-25 08:57:22 -0300

    OT169-51 added 404 response status if activity not found

--- src/main/java/com/alkemy/ong/controller/ActivityController.java
+++ src/main/java/com/alkemy/ong/controller/ActivityController.java
@@ -3,8 +3,10 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.ActivityDto;
 import com.alkemy.ong.service.impl.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -19,8 +21,14 @@ public class ActivityController {
     @PutMapping("/{id}")
     @Validated
     public ActivityDto updateActivity(@PathVariable("id")String id,
-                                      @Valid @RequestBody ActivityDto activityDto) {
-        return service.updateActivity(id, activityDto);
+                                      @Valid @RequestBody ActivityDto activityDto) throws ResponseStatusException {
+        try {
+            return service.updateActivity(id, activityDto);
+        } catch(Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                              String.format("Activity with id %s not found", id));
+        }
+
     }
 
 }
