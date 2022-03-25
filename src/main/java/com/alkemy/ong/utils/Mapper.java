0693b95af38a06a64fe4a699b76c4376f01fb8f2
Hernan <hernan.olmos11@gmail.com>
2022-03-25 17:07:35 -0300

    OT169-50 changed Mapper to receive 2 activity arguments, and updated service

--- src/main/java/com/alkemy/ong/utils/Mapper.java
+++ src/main/java/com/alkemy/ong/utils/Mapper.java
@@ -1,33 +1,20 @@
 package com.alkemy.ong.utils;
 
 import com.alkemy.ong.dto.ActivityDto;
-import com.alkemy.ong.dto.OrganizationRequestDto;
-import com.alkemy.ong.dto.OrganizationResponseDto;
+import com.alkemy.ong.dto.OrganizationDto;
 import com.alkemy.ong.entity.Activity;
 import com.alkemy.ong.entity.OrganizationEntity;
 
 public class Mapper {
 	
-	public static OrganizationResponseDto mapToDto(OrganizationEntity organization, OrganizationResponseDto dto) {
+	public static OrganizationDto mapToDto(OrganizationEntity organization) {
+		OrganizationDto dto = new OrganizationDto();
 		dto.setName(organization.getName());
 		dto.setImage(organization.getImage());
 		dto.setPhone(organization.getPhone());
 		dto.setAddress(organization.getAddress());
 		return dto;
 	}
-
-    public static OrganizationEntity mapFromDto(OrganizationRequestDto dto,
-												OrganizationEntity organization) {
-		organization.setName(dto.getName());
-		organization.setImage(dto.getImage());
-		organization.setEmail(dto.getEmail());
-		organization.setPhone(dto.getPhone());
-		organization.setAddress(dto.getAddress());
-		organization.setWelcomeText(dto.getWelcomeText());
-		organization.setAboutUsText(dto.getAboutUsText());
-		return organization;
-		
-    }
 
 	public static ActivityDto mapToDto(Activity activity, ActivityDto dto) {
 		dto.setId(activity.getId());
@@ -43,4 +30,5 @@ public class Mapper {
 		activity.setImage(dto.getImage());
 		return activity;
 	}
+	
 }
