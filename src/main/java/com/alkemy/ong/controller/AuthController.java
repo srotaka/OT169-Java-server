10f893ecb98833d6d29ec56df1b41fd6e66ab60c
Facundo_98 <facu98ortega@gmail.com>
2022-03-26 16:43:35 -0300

    Jwt logic added

--- src/main/java/com/alkemy/ong/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/controller/AuthController.java
@@ -1,54 +1,29 @@
 package com.alkemy.ong.controller;
 
-import com.alkemy.ong.service.UserService;
+import com.alkemy.ong.dto.AuthenticationResponse;
+import com.alkemy.ong.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.alkemy.ong.entity.User;
-import com.alkemy.ong.repository.UserRepository;
+
 
 @RestController
 @RequestMapping("/auth")
-/**
- * @author Franco Lamberti
- */
 public class AuthController {
-
+	
 	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	
-	@Autowired 
-	private UserRepository userRepository;
+	private AuthService authService;
 
-	@Autowired
-	private UserService userService;
-	
 	@PostMapping("/register")
-	public ResponseEntity<User> register(@RequestBody User user){		
-		userRepository.save(user); 
-		return new ResponseEntity<User>(user, HttpStatus.OK);
+
+	public ResponseEntity<User> register(@RequestBody User user){
+		return authService.register(user);
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<User> login(@RequestParam String mail,@RequestParam String password)  {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		User usuario = new User();
-		try {
-			usuario = userService.findByEmail(mail);
-			if (!encoder.matches(password, usuario.getPassword())){
-				return ResponseEntity.ok().body(null);
-			}
-
-		} catch (Exception e) {
-			e.getMessage();
-		}
-
-		return ResponseEntity.ok(usuario);
-
+	public ResponseEntity<AuthenticationResponse> login(@RequestParam String mail,@RequestParam String password)  throws Exception{
+		return authService.login(mail,password);
 	}
 }
---
+++ src/main/java/com/alkemy/ong/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    private final String jwt;
+}
