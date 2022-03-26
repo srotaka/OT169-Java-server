6b66193f27f85ec2838d75cc66a8f36e1819dcf0
Silvia R. Otaka <srotaka@gmail.com>
2022-03-26 10:21:06 -0300

    Amazon Dependency added in POM

--- src/main/java/com/alkemy/ong/config/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/config/SecurityConfig.java
@@ -24,8 +24,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/organization/public")
+                .antMatchers("/organization/public", "/storage/uploadFile")
                 .hasAnyRole( "ADMIN")
                 .anyRequest()
                 .authenticated()
