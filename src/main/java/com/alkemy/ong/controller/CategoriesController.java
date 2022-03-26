324130535076c60e7e58ecd3b8522acc186d9569
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-03-26 10:26:10 -0300

    CreateCategory Endpoint

--- src/main/java/com/alkemy/ong/controller/CategoriesController.java
+++ src/main/java/com/alkemy/ong/controller/CategoriesController.java
@@ -5,10 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alkemy.ong.entity.Category;
 import com.alkemy.ong.repository.CategoryRepository;
@@ -25,17 +24,13 @@ public class CategoriesController {
 	private CategoryRepository categoryRepository;	
 	/**
 	 * @author Franco Lamberti
-	 * Method only allowed to admins. If the ID is of an existing entity, the method returns it updated version. If it doesn't exists, then returns a 500 error code.
+	 * Method only allowed to admins. The method returns the created entitty. If it doesn't work, then returns a 500 error code.
 	 */
-	@PutMapping("/{id}")//OT169-43
+	@PostMapping("/")//OT169-42
 	@Secured("ROLE_ADMIN")
-	public ResponseEntity<Category> updateCategory(@RequestParam (name = "id") String id, // I get the ID
-			@RequestBody Category category){ //I get the Category to be updated
-		if(categoryRepository.existsById(id)) {//If the category exists			
-			return new ResponseEntity<Category>(categoryRepository.save(category), HttpStatus.OK); //I update it. 
-		}
-		//If it doesn't exists, then I return a 500 error code
-		return new ResponseEntity<Category>(HttpStatus.INTERNAL_SERVER_ERROR);
+	public ResponseEntity<Category> createCategory(@RequestBody Category category){ // I get the entity
+		return new ResponseEntity<Category>(categoryRepository.save(category), HttpStatus.OK); //if it doesn't had errors, return it with a 200 code.
+		
 	}
 	
 	
