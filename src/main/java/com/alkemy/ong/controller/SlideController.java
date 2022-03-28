c5f04313b2425fc6a00e67c55409174aa51732ea
Nico Pistone <npistone@hotmail.com>
2022-03-27 22:40:58 -0300

    Create Slide creation endpoint

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -2,7 +2,9 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.SlideRequestDto;
 import com.alkemy.ong.repository.SlideRepository;
+import com.alkemy.ong.service.SlideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +21,18 @@ public class SlideController {
     @Autowired
     private SlideRepository slideRepository;
 
+    @Autowired
+    private SlideService slideService;
+
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    private void createSlide(@Valid @RequestBody SlideRequestDto slideRequestDto){
+    private ResponseEntity<Void> createSlide(@Valid @RequestBody SlideRequestDto slideRequestDto){
+        try {
+            slideService.createSlide(slideRequestDto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
 
-
-
+       return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
