ea416bcecdddb0cc34fb8f99d568cbc7481ae8a2
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-03-28 11:40:13 -0300

    Changed secure to preAuthorize

--- src/main/java/com/alkemy/ong/controller/CategoriesController.java
+++ src/main/java/com/alkemy/ong/controller/CategoriesController.java
@@ -1,20 +1,14 @@
 package com.alkemy.ong.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alkemy.ong.dto.CategoryDTO;
 import com.alkemy.ong.entity.Category;
 import com.alkemy.ong.repository.CategoryRepository;
 import com.alkemy.ong.service.CategoryService;
@@ -30,7 +24,7 @@ public class CategoriesController {
 	private CategoryService categoryService;
 	
 	@PostMapping("/")//OT169-42
-	//@Secured("ROLE_ADMIN")
+	@PreAuthorize("hasRole('ROLE_ADMIN')") // This method only permits the current role to enter this endpoint
 	public ResponseEntity<Category> createCategory(@RequestBody Category category){ // I get the entity
 		System.out.println("Create");
 		return new ResponseEntity<Category>(categoryService.save(category), HttpStatus.OK); //if it doesn't had errors, return it with a 200 code.
