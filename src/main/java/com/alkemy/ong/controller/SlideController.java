e918481d324ec17a3d97051f0582fddff88a766a
Nico Pistone <npistone@hotmail.com>
2022-03-27 22:18:29 -0300

    Modify SlideServiceImpl, added transform method base64 and logic form create Slide

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -4,6 +4,7 @@ import com.alkemy.ong.dto.SlideRequestDto;
 import com.alkemy.ong.repository.SlideRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +19,9 @@ public class SlideController {
     @Autowired
     private SlideRepository slideRepository;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     private void createSlide(@Valid @RequestBody SlideRequestDto slideRequestDto){
-
-
-
-
-
 
 
 
--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.SlideRequestDto;
+import com.alkemy.ong.entity.OrganizationEntity;
 import com.alkemy.ong.entity.Slide;
+import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.repository.SlideRepository;
 import com.alkemy.ong.service.SlideService;
+import com.alkemy.ong.utils.CustomMultipartFile;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,27 +14,76 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Base64.Encoder;
+import java.util.Optional;
 
 @Service
 public class SlideServiceImpl implements SlideService {
     @Autowired
     private SlideRepository slideRepository;
+    @Autowired
+    private OrganizationRepository organizationRepository;
+    @Autowired
+    private AmazonService amazonService;/*not created yet, branch OT169-31*/
+
+
+
     @Override
     public void createSlide(SlideRequestDto slideRequestDto) throws Exception {
-        /*Llamar AmazonService.uploadFile(pasando multipartFile o file)*/
 
         Slide slide = new Slide();
-        slide.setImageUrl(slideRequestDto.getImgUrl());
-
-        byte[] data = Base64.decodeBase64(slideRequestDto.getImgUrl());
-
-
-        TransformBase64ToMulti transform = new TransformBase64ToMulti(data);
-        transform.transferTo();
-        Integer lugar;
-        if (slideRequestDto.getOrder()==null || slideRequestDto.getOrder().equals("")){
-            slide.setOrder(lugar = Math.toIntExact(slideRepository.count()));
+        Integer dOrder;
+        if (slideRequestDto.getOrder()==null){
+              dOrder= Math.toIntExact(slideRepository.count());
+              slide.setOrder(dOrder);
+        }else {
+            slide.setOrder(slideRequestDto.getOrder());
         }
+        /*
+        * Transform Base64 to MultipartFile, send to AmazonService*/
+        String nameFile = "Slide "+slide.getOrder();
+        MultipartFile imgSend= this.base64ToImage(slideRequestDto.getImgUrl(),nameFile);
+        String amazonUrl=amazonService.uploadFile(imgSend);
+        /*
+        * find organization to add to slide*/
+        Optional<OrganizationEntity> orgFind = organizationRepository.findById(slideRequestDto.getOrganizationId());
+        if (!orgFind.isPresent()){
+            throw new Exception("Organization not found in Slide creation");
+        }else {
+            slide.setOrganizationId(orgFind.get());
+        }
+
+        slide.setText(slideRequestDto.getText());
+
+        slideRepository.save(slide);
+
+
+
+
+
+
+
+    }
+
+    public MultipartFile base64ToImage(String encoded, String fileName) {
+
+        // We need to remove "data:image/jpeg;base64", just to keep the bytes to decode
+        String trimmedEncodedImage = encoded.substring(encoded.indexOf(",") + 1);
+
+        byte[] decodedBytes = Base64.decodeBase64(trimmedEncodedImage);
+
+        CustomMultipartFile customMultipartFile = new CustomMultipartFile(decodedBytes, fileName);
+
+        try {
+            customMultipartFile.transferTo(customMultipartFile.getFile());
+        } catch (IllegalStateException e) {
+            System.out.println("IllegalStateException : " + e);
+        } catch (IOException e) {
+            System.out.println("IOException : " + e);
+        }
+
+        return customMultipartFile;
     }
 }
