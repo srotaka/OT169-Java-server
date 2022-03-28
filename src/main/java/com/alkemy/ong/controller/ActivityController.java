84909b0fd151e1a332e88c38a978ae9057e2ee85
Hernan <hernan.olmos11@gmail.com>
2022-03-28 15:51:20 -0300

    OT169-51 used @PreAuthorize annotation

--- src/main/java/com/alkemy/ong/controller/ActivityController.java
+++ src/main/java/com/alkemy/ong/controller/ActivityController.java
@@ -4,6 +4,7 @@ import com.alkemy.ong.dto.ActivityDto;
 import com.alkemy.ong.service.impl.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,13 +14,14 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/activities")
 @CrossOrigin
+@Validated
 public class ActivityController {
 
     @Autowired
     private ActivityService service;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
-    @Validated
     public ActivityDto updateActivity(@PathVariable("id")String id,
                                       @Valid @RequestBody ActivityDto activityDto) throws ResponseStatusException {
         try {
