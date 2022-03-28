2e0d71c589b08a70cf17ca39081cbeb65c80b765
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-03-28 11:25:35 -0300

    Get category by id endpoint

--- src/main/java/com/alkemy/ong/controller/CategoriesController.java
+++ src/main/java/com/alkemy/ong/controller/CategoriesController.java
@@ -1,36 +1,42 @@
 package com.alkemy.ong.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.alkemy.ong.dto.CategoryDTO;
 import com.alkemy.ong.entity.Category;
 import com.alkemy.ong.repository.CategoryRepository;
+import com.alkemy.ong.service.CategoryService;
 
-@Controller
+@RestController
 @RequestMapping("/categories")
-/**
- * @author Franco Lamberti
- * 
- */
 public class CategoriesController {
 	
 	@Autowired
-	private CategoryRepository categoryRepository;	
-	/**
-	 * @author Lamberti
-	 * Method only allowed to admins. If the ID is of an existing entity, the method returns it. If it doesn't exists, then returns a 404 error code.
-	 */
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private CategoryService categoryService;	
+	
 	@GetMapping("/{id}") //OT169-41
-	@Secured("ROLE_ADMIN")
-	public ResponseEntity<Category> getById(@RequestParam (name = "id") String id) {
+	//@Secured("ROLE_ADMIN")
+	public ResponseEntity<Category> getById(@RequestParam (name = "id", required=false) String id) {
+		System.out.println("Get by id");
+		System.out.println("ID="+id);
+		System.out.println(categoryRepository.existsById(id));
 		if(categoryRepository.existsById(id)){//If the ID corresponds to an Category, returns it
-			return new ResponseEntity<Category>(categoryRepository.getById(id), HttpStatus.OK);
+			return new ResponseEntity<Category>(categoryService.getById(id), HttpStatus.OK);
 		}
 		//If the ID doesn't corresponds to an Category, sends an error
 		return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
