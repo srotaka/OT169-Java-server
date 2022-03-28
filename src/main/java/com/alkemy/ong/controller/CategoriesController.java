7ad3f0b260620096d43f4d2683be1cdd67f9ed7f
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-03-28 11:33:30 -0300

    Delete category endpoint

--- src/main/java/com/alkemy/ong/controller/CategoriesController.java
+++ src/main/java/com/alkemy/ong/controller/CategoriesController.java
@@ -1,44 +1,45 @@
 package com.alkemy.ong.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.alkemy.ong.dto.CategoryDTO;
 import com.alkemy.ong.entity.Category;
 import com.alkemy.ong.repository.CategoryRepository;
+import com.alkemy.ong.service.CategoryService;
 
-@Controller
+@RestController
 @RequestMapping("/categories")
-/**
- * @author Franco Lamberti
- * 
- */
 public class CategoriesController {
 	
 	@Autowired
-	private CategoryRepository categoryRepository;	
-	/**
-	 * @author Franco Lamberti
-	 * Method only allowed to admins. If the ID is of an existing entity, the method returns a 200 code and deletes it. If it doesn't exists, then returns a 500 error code.
-	 */
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private CategoryService categoryService;
+	
 	@DeleteMapping("/{id}")//OT169-44
-	@Secured("ROLE_ADMIN")
+	//@Secured("ROLE_ADMIN")
 	public ResponseEntity<Category> deleteCategory(@RequestParam (name = "id") String id, // I get the ID
 			@RequestBody Category category){ //I get the Category to be deleted
+		System.out.println("Delete");
 		if(categoryRepository.existsById(id)) {//If the category exists
-			categoryRepository.delete(category);// I delete it
+			categoryService.delete(category);// I delete it
 			return new ResponseEntity<Category>(HttpStatus.OK); //I return a 200 code 
 		}
 		//If it doesn't exists, then I return a 500 error code
 		return new ResponseEntity<Category>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-		
-	
 	
 }
