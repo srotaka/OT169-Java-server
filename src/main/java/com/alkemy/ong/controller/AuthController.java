5a451efdf35bef5f39ae2726c9e08c46fbe076cf
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-03-28 11:31:02 -0300

    Update category endpoint

--- src/main/java/com/alkemy/ong/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/controller/AuthController.java
@@ -1,15 +1,20 @@
 package com.alkemy.ong.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alkemy.ong.entity.Category;
 import com.alkemy.ong.entity.User;
+import com.alkemy.ong.repository.CategoryRepository;
 import com.alkemy.ong.repository.RoleRepository;
 import com.alkemy.ong.repository.UserRepository;
 
@@ -29,6 +34,9 @@ public class AuthController {
 	@Autowired 
 	private UserRepository userRepository;
 	
+	@Autowired 
+	private CategoryRepository categoryRepository;
+	
 	@PostMapping("/register")
 	public ResponseEntity<User> register(@RequestBody User user){//recibe un json	
 		System.out.println("Actual user: "+ user);
@@ -42,5 +50,12 @@ public class AuthController {
 		
 		return new ResponseEntity<User>(obj, HttpStatus.OK);//retorna una respuesta que contiene el user creado + el codigo 200
 	}	
+	
+	@GetMapping("/categories")// OT169-40
+	//@Secured("ROLE_ADMIN") // This method only permits the current role to enter this endpoint
+	public List<Category> getNamesFromAll(){
+		System.out.println("Get all");
+		return categoryRepository.getNamesFromAll();
+	}
 	
 }
--- src/main/java/com/alkemy/ong/controller/CategoriesController.java
+++ src/main/java/com/alkemy/ong/controller/CategoriesController.java
@@ -1,43 +1,44 @@
 package com.alkemy.ong.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.alkemy.ong.dto.CategoryDTO;
 import com.alkemy.ong.entity.Category;
 import com.alkemy.ong.repository.CategoryRepository;
+import com.alkemy.ong.service.CategoryService;
 
-@Controller
+@RestController
 @RequestMapping("/categories")
-/**
- * @author Franco Lamberti
- * 
- */
 public class CategoriesController {
 	
 	@Autowired
-	private CategoryRepository categoryRepository;	
-	/**
-	 * @author Franco Lamberti
-	 * Method only allowed to admins. If the ID is of an existing entity, the method returns it updated version. If it doesn't exists, then returns a 500 error code.
-	 */
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private CategoryService categoryService;
+
 	@PutMapping("/{id}")//OT169-43
-	@Secured("ROLE_ADMIN")
+	//@Secured("ROLE_ADMIN")
 	public ResponseEntity<Category> updateCategory(@RequestParam (name = "id") String id, // I get the ID
 			@RequestBody Category category){ //I get the Category to be updated
+		System.out.println("Update");
 		if(categoryRepository.existsById(id)) {//If the category exists			
-			return new ResponseEntity<Category>(categoryRepository.save(category), HttpStatus.OK); //I update it. 
+			return new ResponseEntity<Category>(categoryService.save(category), HttpStatus.OK); //I update it. 
 		}
 		//If it doesn't exists, then I return a 500 error code
 		return new ResponseEntity<Category>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
-	
 	
 }
