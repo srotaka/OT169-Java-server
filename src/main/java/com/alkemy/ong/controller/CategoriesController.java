f20144a8080a481e2602dc53142f89c5aeed4859
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-03-28 11:20:57 -0300

    Not secured Endpoint

--- src/main/java/com/alkemy/ong/controller/CategoriesController.java
+++ src/main/java/com/alkemy/ong/controller/CategoriesController.java
@@ -3,19 +3,12 @@ package com.alkemy.ong.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alkemy.ong.dto.CategoryDTO;
-import com.alkemy.ong.entity.Category;
 import com.alkemy.ong.repository.CategoryRepository;
 import com.alkemy.ong.service.CategoryService;
 
@@ -31,7 +24,7 @@ public class CategoriesController {
 
 	/**
 	 * @author Franco Lamberti
-	 * This method is only allowed to Admins. Returns all the categories.
+	 * This method is only allowed to Admins. Returns all the names of the categories.
 	 */
 	@GetMapping("/")// OT169-40
 	//@Secured("ROLE_ADMIN") // This method only permits the current role to enter this endpoint
