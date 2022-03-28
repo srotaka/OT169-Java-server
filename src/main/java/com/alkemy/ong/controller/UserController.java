992cc5b8dadaf11b0a6f3b5e34bbb1b3571a2fdf
“Camila <camilaguz54@gmail.com>
2022-03-28 08:57:38 -0300

    endpoint to delete user

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -2,20 +2,26 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserService  userService;
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<?> updateUser(@PathVariable String id, @RequestBody Map<Object, Object> fields){
-        return userService.updatePartialInfo(id,fields);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable  String id) {
+        this.userService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
+
+
+
 }
