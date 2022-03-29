75de60ec9ae30fe1872eca3e0e50baa24bf73131
Hernan <hernan.olmos11@gmail.com>
2022-03-28 21:24:57 -0300

    fixed conflicts

--- src/main/java/com/alkemy/ong/service/IActivityService.java
+++ src/main/java/com/alkemy/ong/service/IActivityService.java
@@ -6,4 +6,5 @@ import com.alkemy.ong.entity.Activity;
 public interface IActivityService {
 
     ActivityDto addActivity(ActivityDto activity);
+    ActivityDto updateActivity(String id, ActivityDto activityDto);
 }
