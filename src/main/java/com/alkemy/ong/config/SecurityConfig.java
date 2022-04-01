44fad012d50cbaecf66bb315e73acc93151768dc
Hernan <hernan.olmos11@gmail.com>
2022-04-01 16:50:56 -0300

    OT169-75 secured /comments endpoint

--- src/main/java/com/alkemy/ong/config/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/config/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"/categories/{id}").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST,"/storage/uploadFile").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE,"/storage/deleteFile").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/comments").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and().exceptionHandling()
                 .and().sessionManagement()
