70af15ec945a2bac99964489b5e76bbefc278398
Silvia R. Otaka <srotaka@gmail.com>
2022-04-03 12:42:18 -0300

    Changed List to Map

--- src/main/java/com/alkemy/ong/service/MemberService.java
+++ src/main/java/com/alkemy/ong/service/MemberService.java
@@ -4,12 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import com.alkemy.ong.entity.Member;
 import com.alkemy.ong.repository.MemberRepository;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class MemberService {
@@ -30,16 +27,15 @@ public class MemberService {
 	//List all
 	public ResponseEntity<?> getAllMembers(){
 		List<Member> memberList = memberRepository.findAll();
-		List<String> memberInfolist = new ArrayList<>();
+		Map<String, String> memberInfoMap = new LinkedHashMap<>();
 
 		if(memberList.size() < 0 || memberList.isEmpty()){
 			return ResponseEntity.noContent().build();
 		}
 		for (Member member: memberList) {
-			memberInfolist.add(member.getName());
-			memberInfolist.add(member.getDescription());
+			memberInfoMap.put(member.getName(), member.getDescription());
 		}
-		return ResponseEntity.ok(memberInfolist);
+		return ResponseEntity.ok(memberInfoMap);
 	}
 
 	//Exists
