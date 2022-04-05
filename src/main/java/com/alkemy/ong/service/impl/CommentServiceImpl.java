2d171d4d6663e7c60e5cf874ff80da31cfc8934b
Josue Solis <josue1dario2solis3@gmail.com>
2022-04-04 23:41:01 -0300

    transactional

--- src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 <<<<<<< HEAD
 import java.util.ArrayList;
@@ -76,6 +77,7 @@ public class CommentServiceImpl implements ICommentService {
     }
 
     @Override
+    @Transactional
     public ResponseEntity<Void> delete(String id){
         if(!repository.existsById(id)){
             return ResponseEntity.notFound().build();//Valida si existe el comentario
