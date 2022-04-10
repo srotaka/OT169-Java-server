0dff99cb5e75f8d852f720ecddbaa1b91e097a5e
Silvia R. Otaka <srotaka@gmail.com>
2022-04-10 12:06:29 -0300

    Update TestimonialControllerTest.java

--- src/test/java/com/alkemy/ong/controller/TestimonialControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/TestimonialControllerTest.java
@@ -1,30 +1,34 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.TestimonialDto;
+import com.alkemy.ong.entity.Testimonial;
 import com.alkemy.ong.repository.TestimonialRepository;
 import com.alkemy.ong.service.impl.TestimonialServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.server.ResponseStatusException;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -53,6 +57,9 @@ class TestimonialControllerTest {
     private final String URL = "/testimonials";
 
     TestimonialDto testimonialDto = new TestimonialDto();
+    Testimonial testimonialEntity = new Testimonial();
+    Testimonial testimonialEntity2 = new Testimonial();
+    List<Testimonial> testimonialList = new ArrayList<>();
 
     @BeforeEach
     public void setup(){
@@ -64,6 +71,19 @@ class TestimonialControllerTest {
         testimonialDto.setName("Testimonial Name");
         testimonialDto.setImage("http://aws.com/img01.jpg");
         testimonialDto.setContent("Some content text");
+
+        testimonialEntity.setId("abc123");
+        testimonialEntity.setName("Testimonial Name");
+        testimonialEntity.setImage("http://aws.com/img01.jpg");
+        testimonialEntity.setContent("Some content text");
+
+        testimonialEntity2.setId("abc1234");
+        testimonialEntity2.setName("Testimonial Name");
+        testimonialEntity2.setImage("http://aws.com/img01.jpg");
+        testimonialEntity2.setContent("Some content text");
+
+        testimonialList.add(testimonialEntity);
+        testimonialList.add(testimonialEntity2);
 
     }
     /* ======================================
@@ -125,13 +145,12 @@ class TestimonialControllerTest {
     @WithMockUser(roles = "ADMIN")
     void deleteTestimonial__Success() throws Exception {
 
-        testimonialService.delete("abc123");
+        String urlToDelete = String.format("%s/%s", URL, testimonialList.get(0).getId());
 
-        mockMvc.perform(delete(URL+"/abc123")
-                        .contentType(APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(testimonialDto))
-                        .with(csrf()))
+        mockMvc.perform(delete(urlToDelete)
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
+        assertTrue(testimonialList.get(0).isSoftDelete());
 
     }
 
