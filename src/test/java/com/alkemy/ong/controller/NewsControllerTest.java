2b61b1cbe4b933fe514e950fddc8a65daeb0baf6
“Camila <camilaguz54@gmail.com>
2022-04-12 09:37:16 -0300

    changes in test

--- src/test/java/com/alkemy/ong/controller/NewsControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/NewsControllerTest.java
@@ -1,30 +1,53 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.CategoryBasicDto;
+import com.alkemy.ong.dto.NewsDto;
+import com.alkemy.ong.entity.Category;
 import com.alkemy.ong.entity.News;
 import com.alkemy.ong.repository.NewsRepository;
+import com.alkemy.ong.service.NewsService;
 import com.alkemy.ong.service.impl.NewsServiceImpl;
+import com.alkemy.ong.utils.Mapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hibernate.validator.constraints.URL;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+import static java.util.Arrays.asList;
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,20 +56,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class NewsControllerTest{
 
-        @Autowired
-        protected MockMvc mockMvc;
-
-        @Autowired
-        private NewsRepository newsRepository;
-
-        @Mock
-        private NewsServiceImpl newsServiceImpl;
-
-         @Autowired
-         ObjectMapper objectMapper;
+    private final String URL = "/news";
 
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private NewsRepository newsRepository;
+
+    @MockBean
+    private NewsService newsService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private News news;
+    private NewsDto newsDto;
+    private List<CategoryBasicDto>categories = new ArrayList<CategoryBasicDto>();
 
 
 
@@ -56,88 +85,186 @@ class NewsControllerTest{
                 .apply(springSecurity())
                 .build();
 
+        news = new News();
+        news.setId("123abc");
+        news.setName("name");
+        news.setImage("image");
+        news.setContent("content");
+        news.setSoftDelete(false);
+
+
+        newsDto = new NewsDto();
+        newsDto.setName("name");
+        newsDto.setImage("image");
+        newsDto.setContent("content");
+        categories.add(new CategoryBasicDto("name"));
+        newsDto.setCategories(categories);
 
     }
-
+    //POST//
 
     @Test
-    @WithMockUser(username = "ADMIN" , roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
     void createNews_Ok() throws Exception {
+        when(newsService.save(newsDto)).thenReturn(newsDto);
+        mockMvc.perform(post(URL)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newsDto))
+                        .with(user("admin").roles("ADMIN"))
+                        .with(csrf()))
+                .andExpect(status().isCreated());
 
-      News news = new News();
-        news.setId("01");
-        news.setImage("soy una imagen");
-        news.setName("nombre");
-        news.setContent("content");
-        //VER QUE HAGO CON EL CATEGORIES
-        news = newsRepository.save(news);
-
-         //   when(newsRepository.save(news)).thenReturn(news);
-
-       mockMvc.perform(post("/news")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isForbidden());
-        //RESULTADO:  OK
     }
 
     @Test
     @WithMockUser(username = "ADMIN" , roles = {"ADMIN"})
     void createNews_BadRequest() throws Exception{
-        News news = new News();
-        news.setId("01"); //SETEO EL ID???????
-        news.setImage(null);
-        news.setName(null);
-        news.setContent(null);
-        //news = newsRepository.save(news);
 
-
-        mockMvc.perform(post("/news")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(news)))
+        newsDto.setName(null);
+        mockMvc.perform(post(URL)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newsDto)))
                 .andExpect(status().isBadRequest());
-        // RESULTADO: ME DICE QUE ESPERA UN 400 PERO DEVUELVE UN 403.
+
+
+    }
+    @Test
+    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    void createNews_FailBecauseUserIsNotAdmin() throws Exception {
+        when(newsService.save(newsDto)).thenReturn(newsDto);
+        mockMvc.perform(post(URL)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newsDto))
+                        .with(user("user").roles("USER"))
+                        .with(csrf()))
+                .andExpect(status().isForbidden());
+
     }
 
-
+    //GET//
 
     @Test
-    void getNewsById() {
+    @WithMockUser(username = "ADMIN" , roles = {"ADMIN"})
+    void getNewsById_Ok() throws Exception {
+        String id = "123abc";
+        when(newsService.getNewsById("123abc")).thenReturn(newsDto);
+
+        mockMvc.perform(get("/news/{id}", "123abc")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
 
     }
 
     @Test
-    void updateNews() {
+    @WithMockUser(username = "USER" , roles = {"USER"})
+    void getAllPage_Ok() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/news/pages".concat("?page=")))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
+    @Test
+    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    void getAllPage_Forbidden()throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/news/pages".concat("?page=")))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    //PUT//
+
+    @Test
+    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    void updateNews_Ok() throws Exception {
+        String id = "123abc";
+
+        when(newsService.updateNews("123abc", newsDto)).thenReturn(newsDto);
+        mockMvc.perform(put(URL+"/123abc")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newsDto))
+                        .with(user("admin").roles("ADMIN"))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("name"))
+                .andExpect(jsonPath("$.image").value("image"))
+                .andExpect(jsonPath("$.content").value("content"));
+    }
+
+    @Test
+    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    void updateNews_NotFoundById() throws Exception{
+        String idNotFound = "456def";
+        when(newsService.updateNews(idNotFound, newsDto)).thenThrow((new RuntimeException()));
+        mockMvc.perform(put(URL+idNotFound)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newsDto)))
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    void updateNews_FailBecauseUserIsNotAdmin() throws Exception{
+        when(newsService.updateNews("123abc", newsDto)).thenReturn(newsDto);
+        mockMvc.perform(put(URL + "/123abc")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newsDto))
+                        .with(user("user").roles("USER"))
+                        .with(csrf()))
+                .andExpect(status().isForbidden());
+
+    }
+
+    @Test
+    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    void updateNews_BadRequest() throws Exception{
+        newsDto.setName(null);
+        newsDto.setImage(null);
+        mockMvc.perform(put(URL + "/123abc")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newsDto))
+                        .with(user("admin").roles("ADMIN"))
+                        .with(csrf()))
+                .andExpect(status().isBadRequest());
+
+    }
+
+
+
+    //DELETE//
 
     @Test
     @WithMockUser(username = "ADMIN" , roles = {"ADMIN"})
     void delete_Ok() throws Exception{
-        News news = new News();
-        news.setId("123abc");
+        newsService.delete("123abc");
 
-         //when(newsRepository.findById(news.getId())).thenReturn(Optional.of(news));
+        mockMvc.perform(MockMvcRequestBuilders.delete(URL + "/123abc")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk());
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/news/{id}",20))
-               .andExpect(status().isOk());
 
-            /*RESULTADO: espera un 200 devuelve un 404*/
-
-             /*News news = new News();
-              newsRepository.delete(news);
-                 mockMvc.perform(delete(URL+"/" + news.getId()))
-                         .andExpect(status().isOk());*/
     }
 
     @Test
     @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
     void delete_NotFound() throws Exception{
-        News news = new News();
-        news.setId("123abc");
+        String idNotFound = "456def";
+        newsService.delete("456def");
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/news/{id}", news.getId()))
+        mockMvc.perform(MockMvcRequestBuilders.delete(URL +idNotFound))
                 .andExpect(status().isNotFound());
 
-            //resultado: ok ???????????????
+    }
+
+    @Test
+    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    void delete_FailBecauseUserIsNotAdmin() throws Exception {
+        newsService.delete("123abc");
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(URL + "/123abc")
+                        .contentType(APPLICATION_JSON)
+                        .with(user("user").roles("USER"))
+                        .with(csrf()))
+                .andExpect(status().isForbidden());
+
     }
 
 }
