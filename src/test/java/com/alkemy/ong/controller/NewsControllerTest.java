cdbedd3e657ad647dce1d823279a3f5ee55df3d1
“Camila <camilaguz54@gmail.com>
2022-04-12 19:50:53 -0300

    changes added

--- src/test/java/com/alkemy/ong/controller/NewsControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/NewsControllerTest.java
@@ -2,44 +2,37 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.CategoryBasicDto;
 import com.alkemy.ong.dto.NewsDto;
-import com.alkemy.ong.entity.Category;
+
 import com.alkemy.ong.entity.News;
 import com.alkemy.ong.repository.NewsRepository;
 import com.alkemy.ong.service.NewsService;
-import com.alkemy.ong.service.impl.NewsServiceImpl;
-import com.alkemy.ong.utils.Mapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.amazonaws.services.simplesystemsmanagement.model.ParameterNotFoundException;
+import com.amazonaws.services.simpleworkflow.flow.annotations.Workflow;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hibernate.validator.constraints.URL;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mock;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import org.springframework.test.web.servlet.ResultMatcher;
+
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.server.ResponseStatusException;
 
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-
-import static java.util.Arrays.asList;
-import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+import java.util.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -56,7 +49,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class NewsControllerTest{
 
-    private final String URL = "/news";
 
     @Autowired
     private WebApplicationContext context;
@@ -76,6 +68,9 @@ class NewsControllerTest{
     private News news;
     private NewsDto newsDto;
     private List<CategoryBasicDto>categories = new ArrayList<CategoryBasicDto>();
+    private final static int PAGE = 0;
+    private final static int SIZE = 2;
+    private final String URL = "/news";
 
 
 
@@ -105,6 +100,7 @@ class NewsControllerTest{
 
     @Test
     @WithMockUser(roles = "ADMIN")
+    @DisplayName("Save News Success (Code 201 Created) with role ADMIN")
     void createNews_Ok() throws Exception {
         when(newsService.save(newsDto)).thenReturn(newsDto);
         mockMvc.perform(post(URL)
@@ -117,7 +113,8 @@ class NewsControllerTest{
     }
 
     @Test
-    @WithMockUser(username = "ADMIN" , roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Fail Saving News due to incomplete fields (Error 400 Bad Request)")
     void createNews_BadRequest() throws Exception{
 
         newsDto.setName(null);
@@ -129,7 +126,8 @@ class NewsControllerTest{
 
     }
     @Test
-    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Fail Saving News due to wrong role (Error 403 Forbidden)")
     void createNews_FailBecauseUserIsNotAdmin() throws Exception {
         when(newsService.save(newsDto)).thenReturn(newsDto);
         mockMvc.perform(post(URL)
@@ -144,35 +142,59 @@ class NewsControllerTest{
     //GET//
 
     @Test
-    @WithMockUser(username = "ADMIN" , roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
     void getNewsById_Ok() throws Exception {
         String id = "123abc";
-        when(newsService.getNewsById("123abc")).thenReturn(newsDto);
+        when(newsService.getNewsById(id)).thenReturn(newsDto);
 
-        mockMvc.perform(get("/news/{id}", "123abc")
+        mockMvc.perform(get("/news/{id}", id)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
     }
 
     @Test
-    @WithMockUser(username = "USER" , roles = {"USER"})
-    void getAllPage_Ok() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/news/pages".concat("?page=")))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+    @WithMockUser(roles = "ADMIN")
+    void getNewsById_NotFound() throws Exception{
+        String idNotFound = "456def";
+
+        when(newsService.getNewsById(idNotFound)).thenThrow((new ParameterNotFoundException("")));
+        mockMvc.perform(get(URL+idNotFound)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newsDto)))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    void getAllPage_Ok() throws Exception {
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        when(newsService.getAllPages(0)).thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(URL+"/pages?page")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(response)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
     void getAllPage_Forbidden()throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/news/pages".concat("?page=")))
-                .andExpect(MockMvcResultMatchers.status().isForbidden());
+        Map<String, Object> response = new LinkedHashMap<>();
+        when(newsService.getAllPages(0)).thenThrow(new Exception("Fail to load pages"));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(URL+"/pages?page")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is5xxServerError());
     }
 
     //PUT//
 
     @Test
-    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Update News: Success (Code 200 OK)")
     void updateNews_Ok() throws Exception {
         String id = "123abc";
 
@@ -189,10 +211,11 @@ class NewsControllerTest{
     }
 
     @Test
-    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Fail Updating News due to non-existent ID (Error 404 Not Found)")
     void updateNews_NotFoundById() throws Exception{
         String idNotFound = "456def";
-        when(newsService.updateNews(idNotFound, newsDto)).thenThrow((new RuntimeException()));
+        when(newsService.updateNews(idNotFound, newsDto)).thenThrow((new ParameterNotFoundException("")));
         mockMvc.perform(put(URL+idNotFound)
                         .contentType(APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(newsDto)))
@@ -201,7 +224,8 @@ class NewsControllerTest{
     }
 
     @Test
-    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Fail Updating News due to incorrect role (Error 403 Forbidden)")
     void updateNews_FailBecauseUserIsNotAdmin() throws Exception{
         when(newsService.updateNews("123abc", newsDto)).thenReturn(newsDto);
         mockMvc.perform(put(URL + "/123abc")
@@ -214,10 +238,13 @@ class NewsControllerTest{
     }
 
     @Test
-    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    @WithMockUser(roles= "ADMIN")
+    @DisplayName("Fail Updating News due to incomplete fields (Error 400 Bad Request)")
     void updateNews_BadRequest() throws Exception{
         newsDto.setName(null);
         newsDto.setImage(null);
+        newsDto.setContent(null);
+
         mockMvc.perform(put(URL + "/123abc")
                         .contentType(APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(newsDto))
@@ -232,7 +259,8 @@ class NewsControllerTest{
     //DELETE//
 
     @Test
-    @WithMockUser(username = "ADMIN" , roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Delete News: Success (Code 200 OK)")
     void delete_Ok() throws Exception{
         newsService.delete("123abc");
 
@@ -244,7 +272,8 @@ class NewsControllerTest{
     }
 
     @Test
-    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Fail Deleting News due to non-existent ID (Error 404 Not Found)")
     void delete_NotFound() throws Exception{
         String idNotFound = "456def";
         newsService.delete("456def");
@@ -255,7 +284,8 @@ class NewsControllerTest{
     }
 
     @Test
-    @WithMockUser(username = "ADMIN", roles = {"ADMIN"})
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Fail Deleting News due to incorrect user role (Error 403 Forbidden)")
     void delete_FailBecauseUserIsNotAdmin() throws Exception {
         newsService.delete("123abc");
 
