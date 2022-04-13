69e101bb4a3d454b5b59f80fe0d837afe3f3d7d6
Facundo_98 <facu98ortega@gmail.com>
2022-04-13 20:02:50 -0300

    Register ok test and get authenticated user data test added

--- src/test/java/com/alkemy/ong/controller/AuthControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/AuthControllerTest.java
@@ -1,6 +1,13 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.config.SwaggerConfig;
 import com.alkemy.ong.dto.UserCredentialsDto;
+import com.alkemy.ong.dto.UserDto;
+import com.alkemy.ong.entity.Role;
+import com.alkemy.ong.filter.JwtRequestFilter;
+import com.alkemy.ong.repository.RoleRepository;
+import com.alkemy.ong.repository.UserRepository;
+import com.alkemy.ong.service.EmailService;
 import com.alkemy.ong.service.impl.UserDetailsCustomServiceImpl;
 import com.alkemy.ong.utils.JwtUtils;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,29 +16,39 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Collections;
+import java.util.Optional;
 
 import static com.alkemy.ong.controller.ActivitiesTest.asJson;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestPropertySource(locations = "")
 public class AuthControllerTest {
 
     @Autowired
@@ -43,6 +60,12 @@ public class AuthControllerTest {
 
     private User user;
 
+    private com.alkemy.ong.entity.User userEntity;
+
+    private Role roleEntity;
+
+    private UserDto userDto;
+
     private String jwt;
 
     @Autowired
@@ -52,7 +75,19 @@ public class AuthControllerTest {
     private UserDetailsCustomServiceImpl userDetailsCustomService;
 
     @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private RoleRepository roleRepository;
+
+    @MockBean
+    private EmailService emailService;
+
+    @MockBean
     AuthenticationManager authenticationManager;
+
+    @MockBean
+    HttpServletRequest httpServletRequest;
 
     @MockBean
     private UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken;
@@ -72,12 +107,34 @@ public class AuthControllerTest {
         user = new User("eva@mail.com","1234", Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")));
         jwt = jwtUtil.generateToken(user);
 
+        userDto = new UserDto();
+        userDto.setFirstName("Maria Eva");
+        userDto.setLastName("De los angeles");
+        userDto.setPhoto("eva.jpg");
+        userDto.setEmail("eva@mail.com");
+
+        roleEntity = new Role();
+        roleEntity.setId("1");
+        roleEntity.setDescription("Role with administrator privileges");
+        roleEntity.setName("ADMIN");
+
+        userEntity = new com.alkemy.ong.entity.User();
+        userEntity.setId("10");
+        userEntity.setEmail("eva@mail.com");
+        userEntity.setPhoto("eva.jpg");
+        userEntity.setFirstName("Maria Eva");
+        userEntity.setLastName("De los angeles");
+        userEntity.setPassword("1234");
+        userEntity.setRoleId(roleEntity);
     }
 
+    /*==================================
+            TEST FOR USER LOGIN
+     ===================================*/
 
     @Test
-    @DisplayName("Login user with its credentianls and should return a 200 an a authentication response with the jwt")
-    void logiUser__ShouldLoginUserAndReturnOkAndJwt() throws Exception{
+    @DisplayName("User login with correct credentials and should return a 200 an a authentication response with the jwt")
+    void loginUser__ShouldLoginUserAndReturnOkAndJwt() throws Exception{
 
         usernamePasswordAuthenticationToken.setDetails(userCredentialsDto);
 
@@ -92,5 +149,86 @@ public class AuthControllerTest {
                 .andExpect((jsonPath("$.jwt").value(jwt)));
     }
 
+    @Test
+    @DisplayName("User login with incorrect credentials and should return a 400 bad request")
+    void userLogin__shouldReturnBadRequestIfUserCredentialsAreIncorrect() throws Exception{
 
+        userCredentialsDto.setEmail("");
+
+        when(userDetailsCustomService.loadUserByUsername(userCredentialsDto.getEmail())).thenThrow(new BadCredentialsException("Incorrect email or password"));
+
+        mockMvc.perform(post(commonUrl + "/login")
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(asJson(userCredentialsDto)))
+               .andExpect(status().isBadRequest());
+    }
+
+    /*================================================================
+            TEST FOR THE AUTHENTICATED USER TO OBTAIN THEIR DATA
+      ================================================================*/
+
+    @Test
+    @DisplayName("Get authenticated user data should return 200 ok and return user data")
+    @WithMockUser(username = "eva@mail.com",password = "1234",roles = "ADMIN")
+    void getAuthenticatedUserData__shouldReturnOkAndUserData() throws Exception{
+
+        when(userRepository.findByEmail(userDto.getEmail())).thenReturn(userEntity);
+        when(usernamePasswordAuthenticationToken.isAuthenticated()).thenReturn(true);
+
+        mockMvc.perform(get(commonUrl + "/me").header(HttpHeaders.AUTHORIZATION,"Bearer "+jwt)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("Maria Eva"))
+                .andExpect(jsonPath("$.lastName").value("De los angeles"))
+                .andExpect(jsonPath("$.email").value("eva@mail.com"))
+                .andExpect(jsonPath("$.photo").value("eva.jpg"));
+    }
+
+    @Test
+    @DisplayName("Get authneticated user data should return 401 Unauthorized if a valid JWT is missing in the request or user is not logged in")
+    @WithMockUser(username = "eva@mail.com",password = "1234",roles = "ADMIN")
+    void getAuthenticatedUserData__shouldReturn401UnauthorizedIfAValidJwtIsMissing() throws Exception{
+
+        mockMvc.perform(get(commonUrl + "/me").header(HttpHeaders.AUTHORIZATION,jwt))
+                .andExpect(status().isUnauthorized());
+    }
+
+    /*=======================================
+            TEST FOR REGISTER AN USER
+      =======================================*/
+
+    @Test
+    @DisplayName("Register new user should return 200 ok and the jwt in the authentication response")
+    void registerUser__shouldReturn200OkAndJwt() throws Exception{
+
+        when(userRepository.save(userEntity)).thenReturn(null);
+        when(roleRepository.findById(roleEntity.getId())).thenReturn(Optional.of(roleEntity));
+        when(userDetailsCustomService.loadUserByUsername(userEntity.getEmail())).thenReturn(user);
+        when(usernamePasswordAuthenticationToken.isAuthenticated()).thenReturn(true);
+        when(authenticationManager.authenticate(any())).thenReturn(any());
+
+        mockMvc.perform(post(commonUrl +"/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJson(userEntity)))
+                .andExpect(status().isOk())
+                .andExpect((jsonPath("$.jwt").value(jwt)));
+    }
+
+    @Test
+    @DisplayName("Register new user should return 400 bad request if request is not valid")
+    void registerUser__shouldReturn400BadRequestIfRequestIsNotValid() throws Exception{
+
+        userEntity.setFirstName(null);
+
+        when(userRepository.save(userEntity)).thenReturn(null);
+        when(roleRepository.findById(roleEntity.getId())).thenReturn(Optional.of(roleEntity));
+        when(userDetailsCustomService.loadUserByUsername(userEntity.getEmail())).thenReturn(user);
+        when(usernamePasswordAuthenticationToken.isAuthenticated()).thenReturn(true);
+        when(authenticationManager.authenticate(any())).thenReturn(any());
+
+        mockMvc.perform(post(commonUrl +"/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJson(userEntity)))
+                .andExpect(status().isBadRequest());
+    }
 }
