fd6197646e4c380331023c70adc3cfb7c322a27e
Nico Pistone <npistone@hotmail.com>
2022-04-13 01:55:10 -0300

    Agregar documentacion a News

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.NewsDto;
 import com.alkemy.ong.repository.NewsRepository;
 import com.alkemy.ong.service.NewsService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
+@Api("News endpoints")
 @RestController
 @RequestMapping("/news")
 public class NewsController {
@@ -20,15 +22,54 @@ public class NewsController {
     @Autowired
     private NewsService newsService;
 
+    @ApiOperation(value = "Add News and returns the new created ")
+    @ApiResponses( value={
+            @ApiResponse(code = 400, message = "Bad Request / Invalid field"),
+            @ApiResponse(code = 401, message ="Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden" ),
+            @ApiResponse(code = 201, message = "Created", response = NewsDto.class)
+    })
+    @ApiImplicitParams( value = {
+            @ApiImplicitParam (name = "Authorization", value = "Access Token",
+            required = true,
+            allowEmptyValue = false,
+            paramType = "header",
+            dataTypeClass = String.class,
+            example = "Bearer access_token"),
 
-    @PostMapping
-    public ResponseEntity<NewsDto> save(@Valid @RequestBody NewsDto news){
+            @ApiImplicitParam (name = "NewsDto", value = "Entity to create",
+                                required = true,
+                                allowEmptyValue = false,
+                                paramType = "application/json",
+                                dataTypeClass = NewsDto.class)
+    })
+    @PostMapping(produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<NewsDto> save( @Valid @RequestBody NewsDto news){
+
         NewsDto savedNews= newsService.save(news);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedNews);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<NewsDto> getNewsById(@PathVariable String id) throws Exception {
+
+    @ApiOperation(value = "Get News details by ID ")
+    @ApiResponses( value={
+
+            @ApiResponse(code = 401, message ="Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden" ),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 200, message = "Ok", response = NewsDto.class)
+    })
+    @ApiImplicitParams( value = {
+            @ApiImplicitParam (name = "Authorization", value = "Access Token",
+                    required = true,
+                    allowEmptyValue = false,
+                    paramType = "header",
+                    dataTypeClass = String.class,
+                    example = "Bearer access_token")
+    })
+    @GetMapping(value = "/{id}", produces = {"application/json"})
+    public ResponseEntity<NewsDto> getNewsById(@ApiParam (value = "News id found", required = true, allowEmptyValue = false)
+                                                   @PathVariable String id) throws Exception {
         try{
             NewsDto newsDto= newsService.getNewsById(id);
             return ResponseEntity.status(HttpStatus.OK).body(newsDto);
@@ -37,7 +78,35 @@ public class NewsController {
         }
     }
 
-    @PutMapping("/{id}")
+
+    @ApiOperation(value = "Update News if exist ")
+    @ApiResponses( value={
+
+            @ApiResponse(code = 401, message ="Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden" ),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 200, message = "Ok", response = NewsDto.class)
+    })
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "id",
+                    value = "Id of News",
+                    required = true, allowEmptyValue = false,
+                    paramType = "path", dataTypeClass = String.class,
+                    example = "1"),
+            @ApiImplicitParam(name = "Authorization",
+                    value = "Access Token",
+                    required = true,
+                    allowEmptyValue = false,
+                    paramType = "header",
+                    dataTypeClass = String.class,
+                    example = "Bearer access_token"),
+            @ApiImplicitParam(name= "RequestBody",
+                    value = "Update params of News",
+                    required = true,
+                    paramType = "RequestBody",
+                    dataTypeClass = NewsDto.class
+                    )       })
+    @PutMapping(value = "/{id}", produces = {"application/json"}, consumes = {"application/json"})
     public ResponseEntity<NewsDto> updateNews(@PathVariable String id,@Valid @RequestBody NewsDto newsDto) throws ResponseStatusException {
         try{
             newsService.updateNews( id, newsDto);
@@ -48,13 +117,58 @@ public class NewsController {
             }
     }
 
-    @DeleteMapping("/{id}")
+
+
+    @ApiOperation(value = "Delete News if exist ")
+    @ApiResponses( value={
+
+            @ApiResponse(code = 401, message ="Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden" ),
+            @ApiResponse(code = 404, message = "Not Found", response = ResponseStatusException.class),
+            @ApiResponse(code = 200, message = "Ok", response = ResponseEntity.class)
+    })
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "id",
+                    value = "Id of News",
+                    required = true, allowEmptyValue = false,
+                    paramType = "path", dataTypeClass = String.class,
+                    example = "1"),
+            @ApiImplicitParam(name = "Authorization",
+                    value = "Access Token",
+                    required = true,
+                    allowEmptyValue = false,
+                    paramType = "header",
+                    dataTypeClass = String.class,
+                    example = "Bearer access_token")})
+    @DeleteMapping(value = "/{id}", produces = {"application/json"})
     public ResponseEntity<Void>delete(@PathVariable String id) {
        newsService.delete(id);
        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
-    @GetMapping("/pages")
+
+    @ApiOperation(value = "Return News paginated list.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 401, message ="Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden" ),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ResponseEntity.class),
+            @ApiResponse(code = 200, message = "Ok", response = ResponseEntity.class),
+            })
+    @ApiImplicitParams(value = {
+
+            @ApiImplicitParam(name = "Authorization",
+                    value = "Access Token",
+                    required = true,
+                    allowEmptyValue = false,
+                    paramType = "header",
+                    dataTypeClass = String.class,
+                    example = "Bearer access_token"),
+            @ApiImplicitParam(name = "page", value = "Page of the list",
+                    required = true,
+                    paramType = "query",
+                    dataTypeClass = Integer.class,
+                    example = "1"), })
+    @GetMapping(value = "/pages", produces ={"application/json"} )
     public ResponseEntity<Map<String, Object>> getAllPage(@RequestParam(defaultValue = "0") Integer page){
         Map<String, Object> news = new HashMap<>();
         try {
