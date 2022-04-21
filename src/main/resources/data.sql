e61bbc7395de989cb0bfe560bf283d2c20f931fc
“Camila <camilaguz54@gmail.com>
2022-04-21 18:00:01 -0300

    changes in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,3 @@
--- Creación de Base de datos
-CREATE DATABASE IF NOT EXISTS gr169 CHARACTER SET utf8mb4;
-USE gr169;
 
 -- Creación de roles
 INSERT INTO `gr169`.`role` (`id`, `description`, `name`, `soft_deleted`, `timestamp`) SELECT '1', 'Administrador General', 'ADMIN', 0, NOW() WHERE NOT EXISTS (SELECT * FROM role WHERE id = '1');
